ac621eef13d73d401ab51a89e5bbd4766d51e910
Brian Newsom <brian.newsom@colorado.edu>
2014-11-15 02:23:13 -0700

    add payment info to org pg

--- app/scripts/organization.js
+++ app/scripts/organization.js
@@ -12,10 +12,12 @@ function getQueryParams(search) {
 params = getQueryParams(window.location.search);
 $.getJSON( "https://intense-escarpment-3682.herokuapp.com/organization/" + params.id, function( data ) {
 	var orgContactInfo = "Address: " + data.contact_info.address + "<br>" + "Phone: " + data.contact_info.phone + "<br>" + "Website: " + data.contact_info.website;
-
+	var stats = "Maximum Gift: " + data.payment_status.max + "<br>" + "Total Contribution: " + data.payment_status.total + "<br>" + "Number of Contributions: " + data.payment_status.count;
+	
 	document.getElementById('orgName').innerHTML = data.name;
 	document.getElementById('orgDesc').innerHTML = data.description;
  	document.getElementById('orgContactInfo').innerHTML = orgContactInfo;
  	document.getElementById('pubKey').innerHTML = data.public_key;
+ 	document.getElementById('stats').innerHTML = stats;
 	console.log(data)
 });
