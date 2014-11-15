c0192aa812fb43ed7b0ecbb86c311718b26b28e5
Brian Newsom <brian.newsom@colorado.edu>
2014-11-15 01:32:37 -0700

    try links again

--- app/scripts/listOrganizations.js
+++ app/scripts/listOrganizations.js
@@ -2,6 +2,8 @@ $.getJSON( "https://intense-escarpment-3682.herokuapp.com/organization", functio
 	console.log(data)
 	$('#orgs').append("<ul id='list'></ul>");
     for (cnt = 0; cnt < data.length; cnt++) {
-          $("#list").append("<li>" + data[cnt].name + "</li>");
+    	  var bullet = data[cnt].name;
+    	  var link = "<li><a href='https://intense-escarpment-3682.herokuapp.com/organization.html?id=" + data[cnt].id + "'>" + bullet + "</a></li>";
+          $("#list").append(link);
     }
 });
