e3b7b8fa2abb390f3f69bcefb21d1d991018765f
Brian Newsom <brian.newsom@colorado.edu>
2014-11-14 23:21:33 -0700

    add registration js

---
+++ app/scripts/registration.js
@@ -0,0 +1,18 @@
+var name = document.getElementById('name');
+var description = document.getElementById('description');
+var website = document.getElementById('website');
+var phone = document.getElementById('phone');
+var address = document.getElementById('address');
+var public_key = document.getElementById('public_key');
+
+var JSONObj = {};
+var contact_info = {};
+
+contact_info.website = website;
+contact_info.phone = phone;
+contact_info.address = address;
+
+JSONObj.name = name;
+JSONObj.description = description;
+JSONObj.contact_info = contact_info;
+JSONObj.public_key = public_key;
