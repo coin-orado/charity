8b78f4424a96cc2d8685f6b87f3643e988d0479f
Brian Newsom <brian.newsom@colorado.edu>
2014-11-15 03:01:41 -0700

    try fail in get org

--- app/scripts/organization.js
+++ app/scripts/organization.js
@@ -16,22 +16,24 @@ if (params['error'] == true){
 	$('#error').html("Organization not found");
 }
 
-$.getJSON( "https://intense-escarpment-3682.herokuapp.com/organization/" + params.id, function(data) {
-	console.log("successful ajax post");
-		var orgContactInfo = "Address: " + data.contact_info.address + "<br>" + "Phone: " + data.contact_info.phone + "<br>" + "Website: " + data.contact_info.website;
-		var stats = "Maximum Gift: " + data.payment_status.max + "<br>" + "Total Contribution: " + data.payment_status.total + "<br>" + "Number of Contributions: " + data.payment_status.count;
+else{
+	$.getJSON( "https://intense-escarpment-3682.herokuapp.com/organization/" + params.id, function(data) {
+		console.log("successful ajax post");
+			var orgContactInfo = "Address: " + data.contact_info.address + "<br>" + "Phone: " + data.contact_info.phone + "<br>" + "Website: " + data.contact_info.website;
+			var stats = "Maximum Gift: " + data.payment_status.max + "<br>" + "Total Contribution: " + data.payment_status.total + "<br>" + "Number of Contributions: " + data.payment_status.count;
+				
+			document.getElementById('orgName').innerHTML = data.name;
+			document.getElementById('orgDesc').innerHTML = data.description;
+			document.getElementById('orgContactInfo').innerHTML = orgContactInfo;
+			document.getElementById('pubKey').innerHTML = data.public_key;
+			document.getElementById('stats').innerHTML = stats;
+			console.log(data)
 			
-		document.getElementById('orgName').innerHTML = data.name;
-		document.getElementById('orgDesc').innerHTML = data.description;
-		document.getElementById('orgContactInfo').innerHTML = orgContactInfo;
-		document.getElementById('pubKey').innerHTML = data.public_key;
-		document.getElementById('stats').innerHTML = stats;
-		console.log(data)
-		
-	}).done(function(){
-		alert("success")
-	}).fail(function() { 
-		console.log("error in ajax post");
-		window.location.search = "error=true"
-});
+		}).done(function(){
+			alert("success")
+		}).fail(function() { 
+			console.log("error in ajax post");
+			window.location.search = "error=true"
+	});
+}
 		
