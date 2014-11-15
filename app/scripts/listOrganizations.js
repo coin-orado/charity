dde14dad336695cd6c56bda59561961018f37b78
Brian Newsom <brian.newsom@colorado.edu>
2014-11-15 01:24:19 -0700

    remove link

--- app/scripts/listOrganizations.js
+++ app/scripts/listOrganizations.js
@@ -1,10 +1,7 @@
-var 
 $.getJSON( "https://intense-escarpment-3682.herokuapp.com/organization", function( data ) {
 	console.log(data)
 	$('#orgs').append("<ul id='list'></ul>");
     for (cnt = 0; cnt < data.length; cnt++) {
-    		var link = "<li><a href='https://intense-escarpment-3682.herokuapp.com/organization/?id=" + data[cnt].id + "'>" + data[cnt].name+"</a></li>";
-          console.log(link);
-          $("#list").append(link);
+          $("#list").append("<li>" + data[cnt].name + "</li>");
     }
 });
