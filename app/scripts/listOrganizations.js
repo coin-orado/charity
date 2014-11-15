71933d0eae5d6123bf37ab5385f7ee8e55f1e351
Brian Newsom <brian.newsom@colorado.edu>
2014-11-15 01:19:46 -0700

    debugging links

--- app/scripts/listOrganizations.js
+++ app/scripts/listOrganizations.js
@@ -3,6 +3,8 @@ $.getJSON( "https://intense-escarpment-3682.herokuapp.com/organization", functio
 	console.log(data)
 	$('#orgs').append("<ul id='list'></ul>");
     for (cnt = 0; cnt < data.length; cnt++) {
-          $("#list").append("<li><a href=https://intense-escarpment-3682.herokuapp.com/organization/?id=" + data[cnt].id + ">" + data[cnt].name+"</a></li>");
+    		var link = "<li><a href=https://intense-escarpment-3682.herokuapp.com/organization/?id=" + data[cnt].id + ">" + data[cnt].name+"</a></li>";
+          console.log(link);
+          $("#list").append(link);
     }
 });
