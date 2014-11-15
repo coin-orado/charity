03106d0d418228c1fcb306762e014c891f45ac26
Brian Newsom <brian.newsom@colorado.edu>
2014-11-14 22:28:49 -0700

    pull from url

--- app/scripts/organization.js
+++ app/scripts/organization.js
@@ -1,3 +1,18 @@
+function getQueryParams(qs) {
+    qs = qs.split("+").join(" ");
+
+    var params = {}, tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+
+    while (tokens = re.exec(qs)) {
+        params[decodeURIComponent(tokens[1])]
+            = decodeURIComponent(tokens[2]);
+    }
+
+    return params;
+}
+
+console.log(params);
 $.getJSON( "https://intense-escarpment-3682.herokuapp.com/organization/1", function( data ) {
   // var items = [];
   // $.each( data, function( key, val ) {
