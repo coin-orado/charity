6d6207d169caacc768f5486547e4d937e46f7b29
Brian Newsom <brian.newsom@colorado.edu>
2014-11-14 21:39:47 -0700

    beginning org page

---
+++ app/scripts/organization.js
@@ -0,0 +1,11 @@
+$.getJSON( "https://intense-escarpment-3682.herokuapp.com/1", function( data ) {
+  var items = [];
+  $.each( data, function( key, val ) {
+    items.push( "<li id='" + key + "'>" + val + "</li>" );
+  });
+ 
+  $( "<ul/>", {
+    "class": "my-new-list",
+    html: items.join( "" )
+  }).appendTo( "body" );
+});
