c2f4e2f0c6b405ddb2688e3c908e8e8613e15b3a
Peyman Mortazavi <pmortazavi@cailmobility.com>
2014-11-14 20:09:52 -0700

    added GET /organization/{id} with dummy data

--- Backend/program.js
+++ Backend/program.js
@@ -1,9 +1,38 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+function StartServer() {
 
-app.get("/test", function (require, response) {
+	app.set('port', (process.env.PORT || 5000));
+	app.use(express.static(__dirname + '/'))
+	app.use(bodyParser.json())
 
+	app.listen(app.get('port'), function() {
+	  console.log("Charity server is running at localhost:" + app.get('port'))
+	})
+
+}
+
+StartServer();
+
+
+app.get("/organization/:id", function (request, response) 
+{
+	var dummyOrganizationObject = 
+	{
+		name: "Organization 1",
+		description: "Organization description, we're awesome !",
+		contact_info: 
+		{
+			website: "http://www.google.com/",
+			phone: "+1 (571) 263 - 4240",
+			address: "3685 Moorhead Ave. Bouder, CO 80305"
+		},
+		public_key: "blahblahblahfornow :)"
+	}
+
+	response.send(dummyOrganizationObject)
 });
+
