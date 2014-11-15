34f67a7ec18cc6af06ba4ca11837e328aba9a92f
Brian Newsom <brian.newsom@colorado.edu>
2014-11-14 22:04:46 -0700

    update contactinfo

--- app/scripts/organization.js
+++ app/scripts/organization.js
@@ -8,8 +8,10 @@ $.getJSON( "https://intense-escarpment-3682.herokuapp.com/organization/1", funct
   //   "class": "my-new-list",
   //   html: items.join( "" )
   // }).appendTo( "body" );
+	var orgContactInfo = "Address: " + data.contact_info.address + "<br>" + "Phone: " + data.contact_info.phone + "<br>" + "Website: " + data.contact_info.website;
+
 	document.getElementById('orgName').innerHTML = data.name;
 	document.getElementById('orgDesc').innerHTML = data.description;
- 	document.getElementById('orgContactInfo').innerHTML =  data.contact_info.address;
+ 	document.getElementById('orgContactInfo').innerHTML = orgContactInfo;
 	console.log(data)
 });
