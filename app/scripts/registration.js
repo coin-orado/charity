4c9a803b81f96fea07cebe241b61bd1c04f806c1
Brian Newsom <brian.newsom@colorado.edu>
2014-11-14 23:26:26 -0700

    log registration

--- app/scripts/registration.js
+++ app/scripts/registration.js
@@ -1,18 +1,22 @@
-var name = document.getElementById('name');
-var description = document.getElementById('description');
-var website = document.getElementById('website');
-var phone = document.getElementById('phone');
-var address = document.getElementById('address');
-var public_key = document.getElementById('public_key');
+function createOrg(){
+	var name = document.getElementById('name');
+	var description = document.getElementById('description');
+	var website = document.getElementById('website');
+	var phone = document.getElementById('phone');
+	var address = document.getElementById('address');
+	var public_key = document.getElementById('public_key');
 
-var JSONObj = {};
-var contact_info = {};
+	var JSONObj = {};
+	var contact_info = {};
 
-contact_info.website = website;
-contact_info.phone = phone;
-contact_info.address = address;
+	contact_info.website = website;
+	contact_info.phone = phone;
+	contact_info.address = address;
 
-JSONObj.name = name;
-JSONObj.description = description;
-JSONObj.contact_info = contact_info;
-JSONObj.public_key = public_key;
+	JSONObj.name = name;
+	JSONObj.description = description;
+	JSONObj.contact_info = contact_info;
+	JSONObj.public_key = public_key;
+
+	console.log(JSONObj)
+}
